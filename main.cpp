aa10d74ed7e4bffb6700ec53f013f2952a98fcf5
Herless Alvarado <herlessalvarado@gmail.com>
2023-08-25 21:43:09 -0500

    new main

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 
 #include "Camara.h"
+#include "Luz.h"
+#include "Mundo.h"
+#include "Objeto.h"
+#include "vec3.h"
 
 int main() {
-  Camara cam;
-  cam.configurar(3, 60, 800, 600, vec3(0, 5, 50), vec3(0, 0, 0), vec3(0, 1, 0));
-  cam.renderizar();
+  Camara camara;
+  Luz luz(vec3(400, 300, 0), vec3(1, 1, 1));
+  Mundo mundo(camara, luz);
+  mundo.camara.configurar(3, 60, 800, 600, vec3(0, 5, 50), vec3(0, 0, 0),
+                          vec3(0, 1, 0));
+  mundo.camara.renderizar(mundo.luz);
 
   return 0;
 };
