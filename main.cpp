8932d8e536cf199a3b3d9f37829018292a66c143
Herless Alvarado <herlessalvarado@gmail.com>
2023-08-27 19:25:58 -0500

    50 random spheres

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <random>
 
 #include "Camera.h"
 #include "Light.h"
@@ -6,14 +7,23 @@
 #include "World.h"
 #include "vec3.h"
 
+float randomFloat(float min, float max) {
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    std::uniform_real_distribution<float> dis(min, max);
+    return dis(gen);
+}
+
 int main() {
     Camera camera;
     Light light(vec3(400, 300, 0), vec3(1, 1, 1));
     World world(camera, light);
-    world.camera.configure(3, 60, 800, 600, vec3(0, 5, 50), vec3(0, 0, 0),
+    world.camera.configure(3, 60, 800, 600, vec3(0, 0, 100), vec3(0, 0, 0),
                            vec3(0, 1, 0));
-    Sphere sphere(vec3(2, 0, 0), 8, vec3(0, 0, 1));
-    world.addObject(&sphere);
+    for (int i = 0; i < 50; i++) {
+        Sphere *sphere = new Sphere(vec3(randomFloat(-60, 60), randomFloat(-50, 50), 0), randomFloat(1, 5), vec3(0, 0, 1));
+        world.addObject(sphere);
+    }
     world.camera.render(world.light, world.objects);
 
     return 0;
