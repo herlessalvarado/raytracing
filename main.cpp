9c02690a97efa4d5d6e4b195a2f1776882327e17
Herless Alvarado <herlessalvarado@gmail.com>
2023-09-07 17:28:16 -0500

    new display objects

--- main.cpp
+++ main.cpp
@@ -20,16 +20,16 @@ int main() {
     World world(camera, light);
     world.camera.configure(3, 60, 800, 600, vec3(0, 0, 100), vec3(0, 0, 0),
                            vec3(0, 1, 0));
-    // Plane *plane = new Plane(2.0f, vec3(10, 1, -1), vec3(1, 1, 0));
-    // world.addObject(plane);
-    // for (int i = 0; i < 50; i++) {
-    //     Sphere *sphere = new Sphere(vec3(randomFloat(-60, 60), randomFloat(-50, 50), 0), randomFloat(1, 10), vec3(randomFloat(0, 1), randomFloat(0, 1), randomFloat(0, 1)));
-    //     world.addObject(sphere);
-    // }
-    Sphere *sphere = new Sphere(vec3(0, 10, 1), 20, vec3(0, 0, 1));
-    world.addObject(sphere);
-    Sphere *sphere2 = new Sphere(vec3(10, 15, 1), 20, vec3(1, 0, 1));
-    world.addObject(sphere2);
+    Plane *plane = new Plane(vec3(0, -10, -60), vec3(0, 1, 0), vec3(1, 1, 0));
+    world.addObject(plane);
+    for (int i = 0; i < 50; i++) {
+        Sphere *sphere = new Sphere(vec3(randomFloat(-60, 60), randomFloat(10, 40), randomFloat(1, 10)), randomFloat(1, 10), vec3(randomFloat(0, 1), randomFloat(0, 1), randomFloat(0, 1)));
+        world.addObject(sphere);
+    }
+    // Sphere *sphere = new Sphere(vec3(0, 10, 1), 20, vec3(0, 0, 1));
+    // world.addObject(sphere);
+    // Sphere *sphere2 = new Sphere(vec3(10, 15, 1), 20, vec3(1, 0, 1));
+    // world.addObject(sphere2);
     world.camera.render(world.light, world.objects);
 
     return 0;
