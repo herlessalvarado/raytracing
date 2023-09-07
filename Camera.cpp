9dbc8f809ba4daf3876422bcf7bcc9f656de58fb
Herless Alvarado <herlessalvarado@gmail.com>
2023-09-07 16:47:15 -0500

    latest

--- Camera.cpp
+++ Camera.cpp
@@ -1,5 +1,6 @@
 #include "Camera.h"
 
+#include <cmath>
 #include <iostream>
 
 using namespace std;
@@ -37,22 +38,50 @@ void Camera::render(Light& light, vector<Object*> objects) {
             dir.normalize();
             ray.dir = dir;
             color.set(0, 0, 0);
-            // todos los objects con el t mas cercano
+            Object* closestObject = nullptr;
+            float minT = std::numeric_limits<float>::infinity();
+            vec3 minPi = vec3();
+            vec3 minL = vec3();
+            vec3 minN = vec3();
+            bool hasIntersected = false;
             for (auto object : objects) {
                 if (object->intersect(ray, t, Pi, N)) {
-                    vec3 ambient = light.color * object->ka;
-                    vec3 L = light.pos - Pi;
-                    // double distance_to_light = L.modulus();
-                    L.normalize();
-                    float diff = L.dot(N);
-                    vec3 diffuse = light.color * object->kd * max(0.0f, diff);
-                    vec3 R = 2.0f * diff * N - L;
-                    vec3 V = -dir;
-                    vec3 especular = light.color * (object->ks * (pow(max(0.0f, R.dot(V)), object->shininess)));
-                    color = object->color * (diffuse + ambient + especular);
-                    color.max_to_one();
+                    hasIntersected = true;
+                    if (t < minT && t > 0) {
+                        minT = t;
+                        minPi = Pi;
+                        minN = N;
+                        closestObject = object;
+                    }
                 }
             }
+            if (hasIntersected == true) {
+                vec3 ambient = light.color * closestObject->ka;
+                vec3 L = light.pos - Pi;
+                L.normalize();
+                minL = L;  // rs
+                float diff = L.dot(N);
+                vec3 diffuse = light.color * closestObject->kd * max(0.0f, diff);
+                vec3 R = 2.0f * diff * N - L;
+                vec3 V = -dir;
+                vec3 especular = light.color * (closestObject->ks * (pow(max(0.0f, R.dot(V)), closestObject->shininess)));
+                color = closestObject->color * (diffuse + ambient + especular);
+                color.max_to_one();
+            }
+            // if (minT != std::numeric_limits<float>::infinity()) {
+            //     Ray shadowRay;
+            //     shadowRay.ori = minPi + 0.005 * N;
+            //     shadowRay.dir = minL;
+            //     for (auto object : objects) {
+            //         if (object->intersect(shadowRay, t, Pi, N)) {
+            //             float distance_to_light = sqrt(pow(minPi.x - light.pos.x, 2.0) + pow(minPi.y - light.pos.y, 2.0) + pow(minPi.z - light.pos.z, 2.0));
+            //             float distance_to_current_object = sqrt(pow(minPi.x - Pi.x, 2.0) + pow(minPi.y - Pi.y, 2.0) + pow(minPi.z - Pi.z, 2.0));
+            //             if (distance_to_light > distance_to_current_object) {
+            //                 color = closestObject->color * (light.color * closestObject->ka);
+            //             }
+            //         }
+            //     }
+            // }
             (*pImg)(x, h - 1 - y, 0) = (BYTE)(color.x * 255);
             (*pImg)(x, h - 1 - y, 1) = (BYTE)(color.y * 255);
             (*pImg)(x, h - 1 - y, 2) = (BYTE)(color.z * 255);
