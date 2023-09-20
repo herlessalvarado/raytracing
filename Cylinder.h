a0359d8cbf57767baa6033bd855d946e6424349e
Herless Alvarado <herlessalvarado@gmail.com>
2023-09-20 16:12:43 -0500

    update Cylinder constants

--- Cylinder.h
+++ Cylinder.h
@@ -10,8 +10,7 @@ class Cylinder : public Object {
     float radius;
 
     Cylinder(vec3 top, vec3 bottom, float r, vec3 col, std::string type) : top{top}, bottom{bottom}, radius{r}, Object(col, type) {
-        if (type == "glass") kr = 1;
-        if (type == "glass") ior = 1.5;
+        if (type == "glass") kr = 1, ior = 1.5;
         if (type == "water") ior = 1.33;
         if (type == "diamond") ior = 1.8;
         ka = 0.05;
