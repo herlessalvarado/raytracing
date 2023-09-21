32bb4ce6c37aa0ebca9078b5eceb6f08b6ca5e7a
Herless Alvarado <herlessalvarado@gmail.com>
2023-09-21 10:15:28 -0500

    update clamp

--- vec3.cpp
+++ vec3.cpp
@@ -1,11 +1,1 @@
 #include "vec3.h"
-
-float clamp(float menor, float mayor, float valor) {
-    if (valor < menor) {
-        return menor;
-    }
-    if (valor > mayor) {
-        return mayor;
-    }
-    return valor;
-}
