99656097bb3723d35b54956c341418f1cb4868ef
Herless Alvarado <herlessalvarado@gmail.com>
2023-09-23 17:08:29 -0500

    added x to render

--- Camera.h
+++ Camera.h
@@ -24,7 +24,7 @@ class Camera {
     void configure(float _near, float fov, int width, int height, vec3 pos_eye,
                    vec3 center, vec3 up);
 
-    void render(Light& light, vector<Object*> objects);
+    void render(Light& light, vector<Object*> objects, int x);
 
     vec3 lightning(Light& light, vector<Object*> objects, Ray ray, int depth);
 
