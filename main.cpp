17e5727c556d417124cef496c5a9c8328976f4d5
Herless Alvarado <herlessalvarado@gmail.com>
2023-09-24 21:00:52 -0500

    handle new scene

--- main.cpp
+++ main.cpp
@@ -17,32 +17,45 @@ float randomFloat(float min, float max) {
     return dis(gen);
 }
 
-int main() {
-    std::srand(std::time(NULL));
-    std::vector<std::string> types;
-    types.push_back("glass");
-    types.push_back("concrete");
-    Camera camera;
-    Light light(vec3(40, 100, 40), vec3(1, 1, 1));
-    World world(camera, light);
-    world.camera.configure(3, 60, 800, 600, vec3(0, 0, 100), vec3(0, 0, 0),
-                           vec3(0, 1, 0));
-    Plane *wall = new Plane(vec3(0, 0, -60), vec3(0, 0, 1), vec3(1, 1, 1), "concrete");
+void setupObjects(World &world) {
+    Plane *wall = new Plane(vec3(0, 0, -60), vec3(0, 0, 1), vec3(0.82, 0.82, 0.82), "concrete", false);
+    Plane *table = new Plane(vec3(0, -30, 0), vec3(0, 1, 0), vec3(0.5451, 0.2706, 0.0745), "concrete", false);
+    // Cylinder *bottle = new Cylinder(vec3(0, 20, 0), vec3(0, -30, 0), 20, vec3(0, 0.6, 0.6), "glass");
+    // Cylinder *airBottle = new Cylinder(vec3(0, 20, 20), vec3(0, -30, 20), 18, vec3(1, 0, 0), "glass");
+    Sphere *firefly1 = new Sphere(vec3(0, 10, 10), 1, vec3(1, 1, 0), "insect", true);
+    Sphere *firefly2 = new Sphere(vec3(-40, 10, 10), 1, vec3(1, 1, 0), "insect", true);
+    Sphere *firefly3 = new Sphere(vec3(40, 10, 10), 1, vec3(1, 1, 0), "insect", true);
     world.addObject(wall);
-    Plane *table = new Plane(vec3(0, -30, 0), vec3(0, 1, 0), vec3(1, 1, 0), "concrete");
     world.addObject(table);
-    Cylinder *cylinder = new Cylinder(vec3(0, 10, 0), vec3(0, -30, 0), 20, vec3(0, 0.6, 0.6), "glass");
-    world.addObject(cylinder);
-    // for (int i = 0; i < 50; i++) {
-    //     int randomIndex = rand() % 5;
-    //     Sphere *sphere = new Sphere(vec3(randomFloat(-60, 60), randomFloat(5, 50), randomFloat(1, 10)), randomFloat(1, 10), vec3(randomFloat(0, 1), randomFloat(0, 1), randomFloat(0, 1)), types[randomIndex]);
-    //     world.addObject(sphere);
-    // }
-     Sphere *sphere = new Sphere(vec3(-30, 0, 6), 4, vec3(0, 0, 1), "concrete");
-     world.addObject(sphere);
-     Sphere *sphere2 = new Sphere(vec3(-45, -10, 10), 4, vec3(1, 0, 0), "concrete");
-     world.addObject(sphere2);
-    world.camera.render(world.light, world.objects);
+    // world.addObject(bottle);
+    // world.addObject(airBottle);
+    world.addObject(firefly1);
+    world.addObject(firefly2);
+    world.addObject(firefly3);
+}
+
+void setupSphereLights(World &world) {
+    vector<Object *> objects = world.objects;
+    for (auto object : objects) {
+        Sphere *sphere = dynamic_cast<Sphere *>(object);
+        if (sphere != nullptr && sphere->isLight && sphere->type == "insect") {
+            Light light(sphere->center, sphere->color, 0.1);
+            world.addLight(light);
+        }
+    }
+    return;
+}
+
+int main() {
+    Camera camera;
+    World world(camera);
+    for (int i = 0; i < 20; i++) {
+        world.camera.configure(3, 60, 800, 600, vec3(i * 20, i * 20, 100), vec3(0, 0, 0),
+                               vec3(0, 1, 0));
+        setupObjects(world);
+        setupSphereLights(world);
+        world.camera.render(world.lights, world.objects, i);
+    }
 
     return 0;
 };
